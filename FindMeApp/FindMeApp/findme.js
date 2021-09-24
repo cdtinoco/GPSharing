8485468779d9f4ef821a22124f0b0ab398ba301f
cdtinoco <90579675+cdtinoco@users.noreply.github.com>
2021-09-24 16:25:46 -0500

    Update findme.js

--- FindMeApp/FindMeApp/findme.js
+++ FindMeApp/FindMeApp/findme.js
@@ -5,7 +5,7 @@ const dgram = require('dgram');
 const socket = dgram.createSocket('udp4');
 const sys = require('child_process');
 require('dotenv').config();
-//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+//222222222222222
 //ESTO ES UNA PRUEBA
 console.log("Holaaaaaaaaaaaaaaaa");
 //CONFIGURACIÓN.
