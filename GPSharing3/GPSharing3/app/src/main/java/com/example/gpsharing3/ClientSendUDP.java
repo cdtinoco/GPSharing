e9a73d23ab1301e5c02af53d49d39452ae9025a4
Arturo Pantoja García <japantoja@uninorte.edu.co>
2021-10-02 13:14:43 -0500

    Update ClientSendUDP.java

--- GPSharing3/GPSharing3/app/src/main/java/com/example/gpsharing3/ClientSendUDP.java
+++ GPSharing3/GPSharing3/app/src/main/java/com/example/gpsharing3/ClientSendUDP.java
@@ -16,10 +16,15 @@ public class ClientSendUDP implements Runnable {
     public void run() {
         try {
             DatagramSocket udpSocket = new DatagramSocket(50000);
-            InetAddress serverAddr = InetAddress.getByName(MainActivity.ipa.getText().toString());
+            InetAddress serverAddr = InetAddress.getByName("13.58.169.143");
+            InetAddress serverAddr2 = InetAddress.getByName("52.22.89.135");
+            InetAddress serverAddr3 = InetAddress.getByName("3.139.122.213");
             byte[] buf = (MainActivity.miub.getText().toString()).getBytes();
             DatagramPacket packet = new DatagramPacket(buf, buf.length,serverAddr, 50000);
+            DatagramPacket packet2 = new DatagramPacket(buf, buf.length,serverAddr2, 50000);
+            DatagramPacket packet3 = new DatagramPacket(buf, buf.length,serverAddr3, 50000);
             udpSocket.send(packet);
+            udpSocket.send(packet2);
             udpSocket.close();
         } catch (SocketException e) {
             Log.e("Udp:", "Socket Error:", e);
